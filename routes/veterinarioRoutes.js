ded157729819fd42cafead0da3670e8f1b9d4798
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2023-08-16 16:41:14 -0500

    Mensaje descriptivo de los cambios

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const router = express.Router();
+import {registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken,
+nuevoPassword,
+actualizarPerfil,
+actualizarPassword} from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
+
+//public area
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+//private area
+router.get('/perfil', checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil);
+router.put('/actualizar-password', checkAuth, actualizarPassword);
+
+
+export default router;
